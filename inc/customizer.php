a1f3a74afcb762d6f6d5eb5a803c05f1a8a86d04
nishan <nishan.budhathoki94@gmail.com>
2016-08-31 14:52:15 +0545

    Initial integration of theme

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Time Theme Customizer.
+ *
+ * @package Time
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function time_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'time_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function time_customize_preview_js() {
+	wp_enqueue_script( 'time_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'time_customize_preview_js' );
